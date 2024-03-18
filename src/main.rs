93a424e404d1331a978a6955d6879a7f587a5f2c
SpyroL7 <myminimonkey99@gmail.com>
2024-03-18 21:40:34 +0000

    feat: Instead of using aliases, program will use username in square brackets before the first colon of a commit message. This also allows multiple people to be credited for one commit eg '[person1, person2, person3] msg: blah blah'. Filtering can now be toggeled to be case insensitive if required

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,20 @@ use std::collections::HashMap;
 use std::env;
 use std::fs::{self, OpenOptions, File, remove_file};
 use std::io::{self, Write, BufReader, BufRead};
+use regex::Regex;
 
+// TODO test case insensitive filtering and name exclusion (with/without config and A)
 
 const CONFIG: &str = "config";
 
-// TODO add flag to use [user] instead of config or to autopopulate
 fn process_flags(flags: String, args: &mut Vec<String>) -> Result<(), io::Error> {
     let mut used_args = false;
     for f in flags.chars() {
          match f {
             'r' => reset_config()?,
+            'g' => (),  // autogenerate_config()?,
             'd' if !used_args => { used_args = true; delete_alias(args, false)? },
             'a' if !used_args => { used_args = true; add_alias(args)? },
-            // 'e' if !used_args => { used_args = true; extend_alias(args)? },
             'p' if !used_args => { used_args = true; set_path(args)? },
             bad  => println!("Invalid flag/combination: {}", bad),
         };
@@ -26,17 +27,22 @@ fn process_flags(flags: String, args: &mut Vec<String>) -> Result<(), io::Error>
 fn enable_options(flags: String, options: &mut Vec<bool>) -> () {
     for f in flags.chars() {
         match f {
-            'X' => options[0] = true,  // eXclusively use names in config
+            'X' => options[0] = true,  // eXclusively use names in config (can't be ysed with I)
             'P' => options[1] = true,  // manually provide Path rather than use one in config
-            'I' => options[2] = true,  // Ignore config aliases
+            'I' => options[2] = true,  // Ignore config aliases (can't be used with X)
             'F' => options[3] = true,  // Filter by conventional commit (keyword before first :)
+            'C' => options[4] = true,  // makes filter Case insensitive (requires F)
+            'A' => options[5] = true,  // use Autopopulated config
+            'E' => options[6] = true,  // Exclude name/alias
             bad => println!("Invalid flag/combination: {}", bad),
         }
     };
 }
 
 fn main() -> Result<(), Error> {
-    let mut options: Vec<bool> = vec![false; 5];
+    let untagged = "untagged".to_string();
+
+    let mut options: Vec<bool> = vec![false; 7];
     let mut args: Vec<String> = env::args().skip(1).collect();  // skips the first redundant
                                                                 // argument 
 
@@ -58,18 +64,20 @@ fn main() -> Result<(), Error> {
             Err(e) => panic!("Error finding path: {}", e),
         };
         let mut filters = vec![];
+        let mut exclude = vec![];
         if options[1] {
-            if args.len() == 0 {  // maybe we should panic here so you dont also get repo not
-                                     // found error?
+            if args.len() == 0 {  // maybe we should panic here so it doesn't just use saved one
                 println!("No path argument provided");
             } else if args.len() == 1 {
                 path = args.pop().unwrap();
             } else {
                 println!("Too many arguments");
             }
-        } else if options[3] {
+        } else if options[3] || options[4] {
             // TODO find a way to allow multiple flag arguments maybe like -P path -F filter
             filters = args;
+        } else if options[6] {
+            exclude = args;
         }
 
         let repo = match Repository::open(path) {
@@ -91,7 +99,9 @@ fn main() -> Result<(), Error> {
             Err(e) => panic!("Couldn't parse config file: {}", e),
         };
 
-        // TODO filter by conventional commit message (eg. only Feats)
+        let pattern = r"\[([^,\]]+)(?:, ([^,\]]+))*\]";
+        let regex = Regex::new(pattern).unwrap();
+
         let _ = rw.push_head()?;
         for commit in rw.filter_map(|x| x.ok()) {
             let commit_obj = repo.find_commit(commit)?;
@@ -109,19 +119,44 @@ fn main() -> Result<(), Error> {
             };
             
             let mut found = false;
-            if !options[2] {
-                for (alias, names) in &config_map {
-                    if names.contains(&author_name) || author_name == *alias {
-                        if !options[3] || filters.iter().any(|f| data.contains(f)) {
-                            *commit_counter.entry(alias.to_string()).or_insert(0) += 1;
+            let filtered = !options[3] || filters.iter().any(|f| data.contains(f));
+            let case_insensitive = !options[4] || filters.iter().any(|f| data.to_lowercase().contains(&f.to_lowercase()));
+
+            if options[5] {
+                if let Some(captures) = regex.captures(data) {
+                    for cap in captures.iter().skip(1) {
+                        if let Some(author) = cap {
+                            let author_s = author.as_str().to_string();
+                            let excluded = !options[6] || !exclude.contains(&author_s);
+                            if (filtered || case_insensitive) && excluded {
+                                *commit_counter.entry(author_s).or_insert(0) += 1;
+                            }
                         }
-                        found = true;
                     }
-                };
-            }
-            if !found && !options[0] {  // author_name is not an alias or in the confige
-                if !options[3] || filters.iter().any(|f| data.contains(f)) {
-                    *commit_counter.entry(author_name).or_insert(0) += 1;
+                } else {
+                    // choose how to deal with this - maybe ignore or have an unknown
+                    if !options[6] || !exclude.contains(&untagged) {
+                        *commit_counter.entry(untagged.clone()).or_insert(0) += 1;
+                    }
+                }
+            } else {
+                if !options[2] {
+                    for (alias, names) in &config_map {
+                        let alias_s = alias.to_string();
+                        let excluded = !options[6] || !exclude.contains(&alias_s);
+                        if names.contains(&author_name) || author_name == *alias {
+                            if (filtered && case_insensitive) && excluded {
+                                *commit_counter.entry(alias_s).or_insert(0) += 1;
+                            }
+                            found = true;
+                        }
+                    };
+                }
+                if !found && !options[0] {  // author_name is not an alias or in the config
+                    let excluded = !options[6] || !exclude.contains(&author_name);
+                    if filtered && case_insensitive && excluded {
+                        *commit_counter.entry(author_name).or_insert(0) += 1;
+                    }
                 }
             }
         }
@@ -160,24 +195,6 @@ fn add_alias(names: &mut Vec<String>) -> Result<(), io::Error> {
     Ok(())
 }
 
-// TODO add function to save repo path in config, another to accept path as argument
-
-// fn extend_alias(names: &mut Vec<String>) -> Result<(), io::Error> {
-//     let alias = names.remove(0);
-//     match get_names_with_alias(&alias) {
-//         Ok(result) if result.len() ==  0 => { println!("No alias '{}' found to extend", alias); Ok(()) },
-//         _ => {
-//             let mut _config_file = OpenOptions::new()
-//                 .append(true)
-//                 .create(true)
-//                 .open(CONFIG)?;
-// 
-//     // find line starting with alias, add values to names, remove_alias then add_alias with new
-//     // names
-//             Ok(())
-//         }
-//     }
-// }
 
 fn delete_alias(aliases: &mut Vec<String>, quietly: bool) -> Result<(), io::Error> {
     let config_file = OpenOptions::new()
@@ -211,6 +228,76 @@ fn reset_config() -> Result<(), io::Error> {
     println!("Config reset");
     Ok(())
 }
+
+
+// fn autogenerate_config() -> Result<(), io::Error> {
+//     let path = match get_path() {
+//         Ok(path) => path,
+//         Err(e) => panic!("Error finding path: {}", e),
+//     };
+// 
+//     let mut rw = match repo.revwalk() {
+//         Ok(rw) => rw,
+//         Err(e) => {
+//             println!("Error creating revwalk: {}", e);
+//             return Err(e.into());
+//         }
+//     };
+// 
+//     // TODO put this all into main loop instead of seperate copy
+//     let pattern = r"[\s*[(.),*]*\s*]";
+//     let regex = Regex::new(pattern).unwrap();
+// 
+//     let _ = rw.push_head()?;
+//     for commit in rw.filter_map(|x| x.ok()) {
+//         let commit_obj = repo.find_commit(commit)?;
+//         let author_name = match commit_obj.committer().name() { // option str
+//             Some(name) => name.to_string(),
+//             None => "ERROR".to_string(),
+//         };
+// 
+//         let (data, _msg) = match commit_obj.message(){
+//             Some(commit_msg) => match commit_msg.split_once(":") {
+//                 Some((data, msg)) => (data, msg),
+//                 _ => ("", ""),
+//             },
+//             None => ("", ""),
+//         };
+// 
+//         if let Some(captures) = regex.captures(data) {
+//             let mut done = false;
+//             let mut i = 0;
+//             while !done {
+//                 match captures.get(i) {
+//                     Some(author) => (), // add 1 to counter here
+//                     None => done = true,
+//                 }
+//                 i += 1;
+//             }
+//         } else {
+//             // choose how to deal with this - maybe ignore or have an unknown
+//         }
+//         if !options[2] {
+//             for (alias, names) in &config_map {
+//                 if names.contains(&author_name) || author_name == *alias {
+//                     // TODO maybe add option to make filter non case sensitive
+//                     if !options[3] || filters.iter().any(|f| data.contains(f)) {
+//                         *commit_counter.entry(alias.to_string()).or_insert(0) += 1;
+//                     }
+//                     found = true;
+//                 }
+//             };
+//         }
+//         if !found && !options[0] {  // author_name is not an alias or in the confige
+//             if !options[3] || filters.iter().any(|f| data.contains(f)) {
+//                 *commit_counter.entry(author_name).or_insert(0) += 1;
+//             }
+//         }
+//     }
+// 
+//     Ok(())
+// }
+
 
 fn get_map() -> Result<HashMap<String, Vec<String>>, io::Error> { 
     let config_file = OpenOptions::new()
